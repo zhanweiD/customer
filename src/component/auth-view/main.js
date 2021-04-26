d84a67091b9b00eea3f4edd66358e78cb561662c
zhanwei.dzw <zhanwei.dzw@dtwave-inc.com>
2021-04-26 11:30:47 +0800

    权限优化

--- src/component/auth-view/main.js
+++ src/component/auth-view/main.js
@@ -1,15 +1,18 @@
-import intl from 'react-intl-universal'
 
 /**
  * @description 查看页面权限判断
  */
 
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import NoData from '../no-data'
 
 export default PageComponent => {
   function AuthView(props) {
     const [auth, changeAuth] = useState(true)
+    useEffect(() => {
+      const data = window.frameInfo.perms || []
+      // changeAuth(data.find(item => item === props.match.path))
+    }, [])
     if (!auth) {
       return <NoData text="暂无访问权限，请联系管理员开通权限" />
     }
--- src/page-tag-market/main.js
+++ src/page-tag-market/main.js
@@ -2,13 +2,10 @@
  * @description  标签集市
  */
 import {Route, Switch} from 'react-router-dom'
-import {codeInProduct} from '../common/util'
 
 import TagMarket from './page-tag-market' 
 import TagMarketDetail from './page-tag-market-detail'
  
-const prePath = '/market'
-  
 export default () => {
   return (
     <Switch>
