f024ab7ab72493391e25003f18490667401cad99
zhanwei.dzw <zhanwei.dzw@dtwave-inc.com>
2021-04-30 10:54:59 +0800

    画像优化

--- src/page-portrait/search-list.js
+++ src/page-portrait/search-list.js
@@ -1,13 +1,32 @@
+/* eslint-disable no-restricted-syntax */
+import {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import {NoData} from '../component'
 
 const SearchList = ({data, title, color, id}) => {
-  // console.log(data)
+  const [domList, setDomList] = useState(data)
+  const setList = () => {
+    const newData = data.map(item => {
+      const values = []
+      // eslint-disable-next-line no-restricted-syntax
+      // eslint-disable-next-line guard-for-in
+      for (const key in item) {
+        const value = item[key]
+        values.push(value)
+      }
+      item.name = values[0]
+      return item
+    })
+    setDomList(newData)
+  }
+  useEffect(() => {
+    setList()
+  }, [data])
   return (
     <div className="bgf mr16 mt16 search-list">
       <div style={{backgroundColor: color}} className="list-height">{title}</div>
       {
-        data.length ? null : (
+        domList.length ? null : (
           <NoData 
             text="暂无数据" 
             size="small"
@@ -15,21 +34,17 @@ const SearchList = ({data, title, color, id}) => {
         )
       }
       {
-        data.map(item => {
+        domList.map(item => {
           return (
             <div className="dfsa item-content">
-              {/* <div style={{lineHeight: '36px'}}>头像</div> */}
               <div 
                 className="FBH w100 FBJA ml8" 
                 // onClick={() => window.location.href = `${window.__keeper.pathHrefPrefix}/portrait/${item.ident}/${id}`}
               >
-                {/* <div>{`${item.客户姓名} ${item.type}`}</div> */}
-                {/* <div>{item.客户姓名}</div> */}
-                <Link className="w33" target="_blank" to={`/portrait/${item.ident}/${id}`}>{`${item.客户姓名}`}</Link>
+                <Link className="w33" target="_blank" to={`/portrait/${item.ident}/${id}`}>{`${item.name}`}</Link>
                 <div className="c65 w33">{item.手机号}</div>
                 <div className="c65 w33">{item.recentTime}</div>
               </div>
-              {/* <div style={{lineHeight: '36px'}} className="c65">{item.time}</div> */}
             </div>
           )
         })
