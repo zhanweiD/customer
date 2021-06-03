8cf16d2cc418195884c659af2bd5131a498af9a3
zhanwei.dzw <zhanwei.dzw@dtwave-inc.com>
2021-06-03 17:27:30 +0800

    upload

--- src/page-material/main.js
+++ src/page-material/main.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react'
-import {Button, Upload, message} from 'antd'
+import {Button, Upload, message, Modal, Form, Input} from 'antd'
 import {LoadingOutlined, PlusOutlined} from '@ant-design/icons'
 
 export default () => {
   const [loading, setLoading] = useState(false)
+  const [uploadForm] = Form.useForm()
+
   function getBase64(img, callback) {
     const reader = new FileReader()
     reader.addEventListener('load', () => callback(reader.result))
@@ -11,11 +13,17 @@ export default () => {
   }
   
   function beforeUpload(file) {
-    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png'
-    if (!isJpgOrPng) {
-      message.error('You can only upload JPG/PNG file!')
+    console.log(file)
+    const video = document.createElement('video')
+    video.src = URL.createObjectURL(file)
+    video.onloadedmetadata = function () {
+      console.log('长度', video)
     }
-    const isLt2M = file.size / 1024 / 1024 < 2
+    const isJpgOrPng = file.type === 'audio/mpeg'
+    // if (!isJpgOrPng) {
+    //   message.error('You can only upload JPG/PNG file!')
+    // }
+    const isLt2M = file.size / 1024 / 1024 < 6
     if (!isLt2M) {
       message.error('Image must smaller than 2MB!')
     }
@@ -41,21 +49,69 @@ export default () => {
     </div>
   )
 
-  return (
-    <div>
-      <Upload
-        name="avatar"
-        listType="picture-card"
-        className="avatar-uploader"
-        showUploadList={false}
-        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-        beforeUpload={beforeUpload}
-        onChange={handleChange}
-      >
-        {/* {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton} */}
-        {uploadButton}
-      </Upload>
+  const layout = {
+    labelCol: {span: 4},
+    wrapperCol: {span: 20},
+  }
 
-    </div>
+  const handleOk = () => {
+    console.log('ok')
+  }
+  const handleCancel = () => {
+    console.log('cancel')
+  }
+
+  return (
+    <Modal
+      title="上传素材"
+      visible
+      onOk={handleOk}
+      onCancel={handleCancel}
+    >
+      <Form
+        {...layout}
+        name="basic"
+        form={uploadForm}
+      >
+        <Form.Item
+          label="素材名称"
+          name="username"
+          rules={[{required: true, message: 'Please input your username!'}]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="上传素材"
+          name="material"
+          // rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Upload
+            listType="picture-card"
+            className="avatar-uploader"
+            showUploadList={false}
+            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+            beforeUpload={beforeUpload}
+            onChange={handleChange}
+          >
+            {/* {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton} */}
+            {uploadButton}
+          </Upload>
+        </Form.Item>
+        <Form.Item
+          label="素材名称"
+          name="username"
+          rules={[{required: true, message: 'Please input your username!'}]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="素材名称"
+          name="username"
+          rules={[{required: true, message: 'Please input your username!'}]}
+        >
+          <Input />
+        </Form.Item>
+      </Form>
+    </Modal>
   )
 }
