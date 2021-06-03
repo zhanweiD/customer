8637dc9f9003bc6e75660c676dac72ce97cf222f
zhanwei.dzw <zhanwei.dzw@dtwave-inc.com>
2021-06-03 18:03:39 +0800

    upload

--- src/page-material/main.js
+++ src/page-material/main.js
@@ -1,9 +1,38 @@
 import React, {useState} from 'react'
-import {Button, Upload, message, Modal, Form, Input} from 'antd'
+import {Button, Upload, message, Modal, Form, Input, Radio} from 'antd'
 import {LoadingOutlined, PlusOutlined} from '@ant-design/icons'
+
+const layout = {
+  labelCol: {span: 4},
+  wrapperCol: {span: 20},
+}
+const extraText = {
+  picture: '支持jpg/png格式，大小1MB以内',
+  audio: '支持mp3/wma/wav/amr格式，播放长度不超过60s，大小2MB以内',
+  video: '支持mp4格式，大小10MB以内',
+}
+function beforeUpload(file) {
+  console.log(file)
+  const video = document.createElement('video')
+  video.src = URL.createObjectURL(file)
+  video.onloadedmetadata = function () {
+    console.log('长度', video)
+  }
+  const isJpgOrPng = file.type === 'audio/mpeg'
+  // if (!isJpgOrPng) {
+  //   message.error('You can only upload JPG/PNG file!')
+  // }
+  const isLt2M = file.size / 1024 / 1024 < 6
+  if (!isLt2M) {
+    message.error('Image must smaller than 2MB!')
+  }
+  return isJpgOrPng && isLt2M
+}
 
 export default () => {
   const [loading, setLoading] = useState(false)
+  const [materialType, setMaterialType] = useState(1)
+  const [uploadExtra, setUploadExtra] = useState(extraText.picture)
   const [uploadForm] = Form.useForm()
 
   function getBase64(img, callback) {
@@ -12,23 +41,7 @@ export default () => {
     reader.readAsDataURL(img)
   }
   
-  function beforeUpload(file) {
-    console.log(file)
-    const video = document.createElement('video')
-    video.src = URL.createObjectURL(file)
-    video.onloadedmetadata = function () {
-      console.log('长度', video)
-    }
-    const isJpgOrPng = file.type === 'audio/mpeg'
-    // if (!isJpgOrPng) {
-    //   message.error('You can only upload JPG/PNG file!')
-    // }
-    const isLt2M = file.size / 1024 / 1024 < 6
-    if (!isLt2M) {
-      message.error('Image must smaller than 2MB!')
-    }
-    return isJpgOrPng && isLt2M
-  }
+  
   const handleChange = info => {
     console.log(info)
     if (info.file.status === 'uploading') {
@@ -49,16 +62,20 @@ export default () => {
     </div>
   )
 
-  const layout = {
-    labelCol: {span: 4},
-    wrapperCol: {span: 20},
-  }
-
   const handleOk = () => {
     console.log('ok')
   }
   const handleCancel = () => {
     console.log('cancel')
+  }
+  const changeRadio = v => {
+    const {value} = v.target
+    const {audio, video, picture} = extraText
+    console.log(v.target.value)
+    if (value === 1 || value === 2) setUploadExtra(picture)
+    if (value === 3) setUploadExtra(audio)
+    if (value === 4) setUploadExtra(video)
+    setMaterialType(v)
   }
 
   return (
@@ -74,15 +91,22 @@ export default () => {
         form={uploadForm}
       >
         <Form.Item
-          label="素材名称"
-          name="username"
+          label="素材类型"
+          name="materialType"
+          initialValue={materialType}
           rules={[{required: true, message: 'Please input your username!'}]}
         >
-          <Input />
+          <Radio.Group onChange={changeRadio}>
+            <Radio value={1}>图片</Radio>
+            <Radio value={2}>二维码</Radio>
+            <Radio value={3}>音频</Radio>
+            <Radio value={4}>视频</Radio>
+          </Radio.Group>
         </Form.Item>
         <Form.Item
           label="上传素材"
           name="material"
+          extra={uploadExtra}
           // rules={[{ required: true, message: 'Please input your username!' }]}
         >
           <Upload
@@ -99,17 +123,16 @@ export default () => {
         </Form.Item>
         <Form.Item
           label="素材名称"
-          name="username"
-          rules={[{required: true, message: 'Please input your username!'}]}
+          name="name"
+          rules={[{required: true, message: '请输入名称'}]}
         >
-          <Input />
+          <Input placeholder="请输入名称" />
         </Form.Item>
         <Form.Item
-          label="素材名称"
-          name="username"
-          rules={[{required: true, message: 'Please input your username!'}]}
+          label="描述"
+          name="descr"
         >
-          <Input />
+          <Input placeholder="请输入描述" />
         </Form.Item>
       </Form>
     </Modal>
