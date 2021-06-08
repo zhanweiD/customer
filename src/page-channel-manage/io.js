681e0e77d774bf568ce1ecc5be0c87dd22ca5af5
zhanwei.dzw <zhanwei.dzw@dtwave-inc.com>
2021-06-08 11:48:47 +0800

    渠道管理

--- src/page-channel-manage/io.js
+++ src/page-channel-manage/io.js
@@ -2,9 +2,10 @@ import ioContext from '../common/io-context'
 import {baseApi, marketingApi, get, post} from '../common/util'
 
 const api = {
-  getList: post(`${marketingApi}/plan/list`), // 获取列表
-  delPlan: post(`${marketingApi}/plan/delete`), // 删除计划
-  getChannelList: post(`${marketingApi}/plan/getChannelList`), // 渠道列表
+  getList: post(`${marketingApi}/bas-channel-account/page`), // 获取列表
+  getAccount: post(`${marketingApi}/bas-channel-account/count`), // 获取渠道下账号个数
+  setEnable: post(`${marketingApi}/bas-channel-account/enable`), // 启停用
+  authoriza: get(`${marketingApi}/wechat/pc_auth`), // 授权跳转
 } 
 
 ioContext.create('channelManage', api) 
--- src/page-channel-manage/main.js
+++ src/page-channel-manage/main.js
@@ -1,14 +1,14 @@
 import {useEffect, useState} from 'react'
-import {Button, Table, Switch, Spin, Select} from 'antd'
-import {successTip, changeToOptions, errorTip} from '@util'
+import {Button, Table, Tabs, Badge, Select} from 'antd'
+import {successTip, errorTip} from '@util'
 import io from './io'
 
-const {Option} = Select
+const {TabPane} = Tabs
 
 export default () => {
   const [listDate, setListDate] = useState([]) // 表格数据
   const [channelList, setChannelList] = useState([]) // 渠道列表
-  const [searchParam, setSearchParam] = useState({}) // 搜索
+  const [channelCode, setChannelCode] = useState() // channelCode
   const [tableLoading, setTableLoading] = useState(false) // 搜索loading
   const [pagination, setPagination] = useState({
     current: 1,
@@ -23,8 +23,8 @@ export default () => {
       const res = await io.getList({
         currentPage: pagination.current,
         pageSize: pagination.pageSize,
+        channelCode,
         ...params,
-        ...searchParam,
       })
       const {data, currentPage, pageSize, totalCount} = res
       setListDate(data)
@@ -40,104 +40,152 @@ export default () => {
     } 
   }
 
-  // 获取渠道
-  const getChannelList = async () => {
+  // 获取渠道下账号个数
+  const getAccount = async () => {
     try {
-      const res = await io.getChannelList()
-      setChannelList(changeToOptions(res || [])('name', 'id'))
+      const res = await io.getAccount()
+      const item = res[0] || {}
+      setChannelList(res || [])
+      setChannelCode(item.code)
     } catch (error) {
       errorTip(error.message)
     }
   }
 
-  const enable = v => {
-    console.log(v)
+  // 启停用
+  const setEnable = async record => {
+    try {
+      await io.setEnable({
+        enable: !record.enable,
+        id: record.id,
+      })
+      successTip(record.enable ? '暂停成功' : '启用成功')
+      getList()
+    } catch (error) {
+      errorTip(error.message)
+    }
   }
-  const authorization = v => {
-    console.log(v)
+
+  // 授权跳转
+  const authoriza = async () => {
+    try {
+      const res = await io.authoriza()
+      if (res) window.open(res)
+    } catch (error) {
+      errorTip(error.message)
+    }
+  }
+
+  const changeTabs = v => {
+    setChannelCode(v)
+  }
+  const columns = (name, type) => {
+    return (
+      [
+        {
+          title: name,
+          dataIndex: 'accountName',
+          key: 'accountName',
+        },
+        {
+          title: type,
+          dataIndex: 'accountType',
+          key: 'accountType',
+        },
+        {
+          title: '创建人',
+          dataIndex: 'cuserName',
+          key: 'cuserName',
+        },
+        {
+          title: '更新时间',
+          dataIndex: 'mtime',
+          key: 'mtime',
+        },
+        {
+          title: '账号状态',
+          dataIndex: 'accountStatus',
+          key: 'accountStatus',
+          render: text => {
+            if (text) {
+              return <Badge color="green" text="授权成功" />
+            }
+            return <Badge color="default" text="授权失败" />
+          },
+        },
+        {
+          title: '是否启用',
+          dataIndex: 'enable',
+          key: 'enable',
+          render: text => (text ? '启用' : '暂停'),
+        },
+        {
+          title: '操作',
+          key: 'action',
+          render: (text, record) => ([
+            <a onClick={() => setEnable(record)}>{record.enable ? '暂停' : '启用'}</a>,
+            // <a onClick={() => authorization(record)}>重新授权</a>,
+          ]),
+        },
+      ]
+    )
+  }
+  const setTab = item => {
+    const {name} = item
+    let type = ''
+    switch (name) {
+      case '微信公众号':
+        type = '公众号类型'
+        break
+      default:
+        type = '短信'
+        break
+    }
+    return (
+      <Table 
+        columns={columns(name, type)} 
+        dataSource={listDate} 
+        rowClassName={(rowData, index) => `ant-table-row-${index % 2}`}
+        scroll={{x: 720}} 
+        loading={tableLoading}
+        pagination={{
+          ...pagination,
+          showTotal: () => `合计${pagination.total}条记录`,
+          onChange: v => getList({currentPage: v}),
+        }}
+      />
+    )
   }
 
   useEffect(() => {
-    getChannelList()
+    getAccount()
   }, [])
   useEffect(() => {
-    getList({currentPage: 1})
-  }, [searchParam])
-
-  const columns = [
-    {
-      title: '微信公众号名称',
-      dataIndex: 'touchCount',
-      key: 'touchCount',
-    },
-    {
-      title: '公众号类型',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: '创建人',
-      dataIndex: 'targetRate',
-      key: 'targetRate',
-    },
-    {
-      title: '更新时间',
-      dataIndex: 'endTime',
-      key: 'endTime',
-    },
-    {
-      title: '账号状态',
-      dataIndex: 'endTime',
-      key: 'endTime',
-    },
-    {
-      title: '是否启用',
-      dataIndex: 'endTime',
-      key: 'endTime',
-    },
-    {
-      title: '操作',
-      key: 'action',
-      render: (text, record) => ([
-        <a className="mr16" onClick={() => enable(record)}>启用</a>,
-        <a onClick={() => authorization(record)}>重新授权</a>,
-      ]),
-    },
-  ]
+    if (channelCode) getList({currentPage: 1})
+  }, [channelCode])
 
   return (
     <div className="oa">
       <div className="content-header">渠道管理</div>
       <div className="m16 mt72 bgf p16 pt0" style={{minHeight: 'calc(100vh - 137px)'}}>
-        <div className="pt16 mb8 FBH FBJB">
+        <div className="pt16 pr">
           <Button
             type="primary"
-            onClick={() => console.log(111)}
+            style={{position: 'absolute', right: 0, zIndex: 1}}
+            onClick={authoriza}
           >
             授权账号
           </Button>
-          <Select style={{width: 160}} defaultValue="">
-            <Option value="">全部</Option>
-            <Option value="1">微信公众号</Option>
-            <Option value="2">邮件</Option>
-            <Option value="3">短信</Option>
-            <Option value="4">App推送</Option>
-            <Option value="5">WebHook</Option>
-          </Select>
         </div>
-        
-        <Table 
-          columns={columns} 
-          dataSource={listDate} 
-          rowClassName={(rowData, index) => `ant-table-row-${index % 2}`}
-          scroll={{x: 1280}} 
-          loading={tableLoading}
-          pagination={{
-            ...pagination,
-            showTotal: () => `合计${pagination.total}条记录`,
-            onChange: v => getList({currentPage: v}),
-          }}
-        />
+        <Tabs onChange={changeTabs} type="card">
+          {
+            channelList.map(item => (
+              <TabPane tab={`${item.name}(${item.cnt})`} key={item.code}>
+                {setTab(item)}
+              </TabPane>
+            ))
+          }
+        </Tabs>
       </div>
     </div>
   )
